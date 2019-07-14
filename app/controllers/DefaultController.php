c09b8ece04967b898ba3df10f71ee44bacd1b69e
GihanAyesh <gihanayeshmanthadrck@gmail.com>
2019-07-14 14:03:39 +0530

    Home page

--- app/controllers/DefaultController.php
+++ app/controllers/DefaultController.php
@@ -2,5 +2,7 @@
 require_once 'FeedController.php';
 class DefaultController extends Controller
 {
-
+	public function index($params=''){
+		echo file_get_contents(VIEW_PATH. 'home/index.php');
+	}
 }
